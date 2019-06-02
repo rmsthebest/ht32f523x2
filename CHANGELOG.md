114e1c9b9cab9424518beb4979ebc070fd1a0f27
rmsthebest <11652273+rmsthebest@users.noreply.github.com>
2019-06-02 13:34:21 +0200

    svd2rust 0.14.0 and shorter variable names

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+
+## [v0.2.0] 2019-06-02
+- use svd2rust 0.14.0
+- Changed svd to produce shorter variable names.
+For example `gpioa::gpioa_dinr` is now `gpioa::dinr`
+
+## [v0.1.0] 2018-10-4
+Initial release
+
--- src/fault_reports.rs
+++ src/fault_reports.rs
@@ -1,12 +1,1 @@
-#[doc = r" Register block"]
-#[repr(C)]
-pub struct RegisterBlock {
-    #[doc = "0x00 - DFSR"]
-    pub dfsr: DFSR,
-}
-#[doc = "DFSR"]
-pub struct DFSR {
-    register: ::vcell::VolatileCell<u32>,
-}
-#[doc = "DFSR"]
-pub mod dfsr;
+# [ doc = r" Register block" ] # [ repr ( C ) ] pub struct RegisterBlock { # [ doc = "0x00 - DFSR" ] pub dfsr : DFSR } # [ doc = "DFSR" ] pub struct DFSR { register : :: vcell :: VolatileCell < u32 > } # [ doc = "DFSR" ] pub mod dfsr ;
